c95629964d6a07ee7d1dab1e90e0c489d083cf6f
Jo Wagner <jowagner@limago.de>
2024-01-29 11:24:32 +0100

    schwein

--- Tag1_02SchweineProjekt/.idea/sonarlint/issuestore/index.pb
+++ Tag1_02SchweineProjekt/.idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,5 @@
 CMakeLists.txt,9\a\9a2aa4db38d3115ed60da621e012c0efc0172aae
 ?
 source/main.cpp,3\3\33b850e7ffd8f887abe2ff126fdaab4831ec06ad
+F
+source/tiere/Schwein.h,1\d\1d0047291727c47bed420328570749933f96cb8b
---
+++ Tag1_02SchweineProjekt/source/tiere/Schwein.h
@@ -0,0 +1,55 @@
+//
+// Created by JoachimWagner on 29.01.2024.
+//
+
+#pragma once
+
+
+#include <string>
+#include <ostream>
+#include <stdexcept>
+
+class Schwein {
+    std::string name;
+    int gewicht;
+
+    void setGewicht(int gewicht) {
+        Schwein::gewicht = gewicht;
+    }
+public:
+    explicit Schwein(const std::string &name="Nobody") :  gewicht(10) {
+        setName(name);
+    }
+
+    const std::string &getName() const {
+        return name;
+    }
+
+    void setName(const std::string &name) {
+        if(name == "Elsa") throw std::invalid_argument("Elsa ist nicht erlaubt");
+        Schwein::name = name;
+    }
+
+    int getGewicht() const {
+        return gewicht;
+    }
+
+    void fuettern() {
+        setGewicht(getGewicht() + 1);
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const Schwein &schwein) {
+        os << "name: " << schwein.name << " gewicht: " << schwein.gewicht;
+        return os;
+    }
+
+    bool operator==(const Schwein &rhs) const {
+        return name == rhs.name &&
+               gewicht == rhs.gewicht;
+    }
+
+    bool operator!=(const Schwein &rhs) const {
+        return !(rhs == *this);
+    }
+
+};
